43b8abcc1d7e8bee0e6958e45d24b46f5422e42e
Мaксим Алексеев <77208888+alekseevmax26@users.noreply.github.com>
2021-11-11 18:26:12 +0300

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -37,7 +37,7 @@ def browser(request):
     fp = webdriver.FirefoxProfile()
     drivers = request.config.getoption("--drivers")
     logger.info(f"Run browser {browser_name}")
-    if executor == "localhost":
+    if executor == "192.168.0.103":
         capabilities = {'goog:chromeOptions': {}}
 
         if browser_name == "chrome":
